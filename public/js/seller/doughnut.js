90fb565b27c38fc19c428f79e0537f0a757f7256
dezziner93 <saif14497@gmail.com>
2022-10-07 02:33:27 +0500

    working on doughnut

--- public/js/seller/doughnut.js
+++ public/js/seller/doughnut.js
@@ -1,6 +1,25 @@
 let ctx = document.getElementById("myChart").getContext("2d");
-let ctx2 = document.getElementById("myChart2").getContext("2d");
-let ctx3 = document.getElementById("myChart3").getContext("2d");
+const productCategoriesForDoughnut = document.querySelectorAll('.productCategoriesForDoughnut');
+// let ctx2 = document.getElementById("myChart2").getContext("2d");
+// let ctx3 = document.getElementById("myChart3").getContext("2d");
+
+
+let stringForLabels = [];
+
+productCategoriesForDoughnut.forEach(function(category){  
+  stringForLabels.push(category.innerHTML);
+})
+
+let stringForData = [];
+
+stringForLabels.forEach(function (p, i) {
+  stringForData[p] = (stringForData[p] || 0) + 1;
+});
+
+
+let newStringForLabels = new Set(stringForLabels);
+newStringForLabels = [...newStringForLabels];
+
 
 Chart.defaults.global.defaultFontColor = "#888";
 Chart.defaults.global.maintainAspectRatio = true;
@@ -20,7 +39,7 @@ let myChart = new Chart(ctx, {
   type: "pie",
 
   data: {
-    labels: ["Python", "Swift", "Objective-C", "Java", "Others", "CSS"],
+    labels: newStringForLabels,
     datasets: [
       {
         label: "languages",
@@ -41,23 +60,13 @@ let myChart = new Chart(ctx, {
           "#563d7c"
         ],
         borderWidth: 2.4,
-        data: [6, 6, 4, 3, 3, 2],
+        data: [1, 1, 1],
         barThickness: 30
       }
     ]
   },
   options: {
     cutoutPercentage: 65,
-    // responsive: false,
-    // scales: {
-    //   yAxes: [
-    //     {
-    //       ticks: {
-    //         beginAtZero: true
-    //       }
-    //     }
-    //   ]
-    // },
     legend: {
       display: true,
       position: "top",
@@ -77,142 +86,143 @@ let myChart = new Chart(ctx, {
   }
 });
 
-let myChart2 = new Chart(ctx2, {
-  plugins: [
-    {
-      beforeInit: function(chart) {
-        chart.legend.afterFit = function() {
-          this.height = this.height + 20;
-        };
-      }
-    }
-  ],
-  type: "pie",
 
-  data: {
-    labels: ["Python", "Swift", "Objective-C", "Java", "Others", "CSS"],
-    datasets: [
-      {
-        label: "languages",
-        backgroundColor: [
-          `rgba(241, 224, 90, ${t})`,
-          `rgb(86, 61, 124, ${t})`,
-          `rgb(227, 76, 38, ${t})`,
-          `rgb(44, 62, 80, ${t})`,
-          `rgb(43, 116, 137, ${t})`,
-          `rgb(86, 61, 124, ${t})`
-        ],
-        borderColor: [
-          "#f1e05a",
-          "#563d7c",
-          "#e34c26",
-          "#2c3e50",
-          "#2b7489",
-          "#563d7c"
-        ],
-        borderWidth: 2.4,
-        data: [6, 6, 4, 3, 3, 2],
-        barThickness: 30
-      }
-    ]
-  },
-  options: {
-    cutoutPercentage: 65,
-    // responsive: false,
-    // scales: {
-    //   yAxes: [
-    //     {
-    //       ticks: {
-    //         beginAtZero: true
-    //       }
-    //     }
-    //   ]
-    // },
-    legend: {
-      display: true,
-      position: "top",
-      align: "center",
-      labels: {
-        boxWidth: 7,
-        padding: 14,
-        usePointStyle: true
-      }
-    },
-    tooltips: {
-      mode: "point",
-      titleFontColor: "black",
-      bodyFontColor: "black",
-      backgroundColor: "#eee"
-    }
-  }
-});
+// let myChart2 = new Chart(ctx2, {
+//   plugins: [
+//     {
+//       beforeInit: function(chart) {
+//         chart.legend.afterFit = function() {
+//           this.height = this.height + 20;
+//         };
+//       }
+//     }
+//   ],
+//   type: "pie",
 
-let myChart3 = new Chart(ctx3, {
-  plugins: [
-    {
-      beforeInit: function(chart) {
-        chart.legend.afterFit = function() {
-          this.height = this.height + 20;
-        };
-      }
-    }
-  ],
-  type: "pie",
+//   data: {
+//     labels: ["Python", "Swift", "Objective-C", "Java", "Others", "CSS"],
+//     datasets: [
+//       {
+//         label: "languages",
+//         backgroundColor: [
+//           `rgba(241, 224, 90, ${t})`,
+//           `rgb(86, 61, 124, ${t})`,
+//           `rgb(227, 76, 38, ${t})`,
+//           `rgb(44, 62, 80, ${t})`,
+//           `rgb(43, 116, 137, ${t})`,
+//           `rgb(86, 61, 124, ${t})`
+//         ],
+//         borderColor: [
+//           "#f1e05a",
+//           "#563d7c",
+//           "#e34c26",
+//           "#2c3e50",
+//           "#2b7489",
+//           "#563d7c"
+//         ],
+//         borderWidth: 2.4,
+//         data: [6, 6, 4, 3, 3, 2],
+//         barThickness: 30
+//       }
+//     ]
+//   },
+//   options: {
+//     cutoutPercentage: 65,
+//     // responsive: false,
+//     // scales: {
+//     //   yAxes: [
+//     //     {
+//     //       ticks: {
+//     //         beginAtZero: true
+//     //       }
+//     //     }
+//     //   ]
+//     // },
+//     legend: {
+//       display: true,
+//       position: "top",
+//       align: "center",
+//       labels: {
+//         boxWidth: 7,
+//         padding: 14,
+//         usePointStyle: true
+//       }
+//     },
+//     tooltips: {
+//       mode: "point",
+//       titleFontColor: "black",
+//       bodyFontColor: "black",
+//       backgroundColor: "#eee"
+//     }
+//   }
+// });
 
-  data: {
-    labels: ["Python", "Swift", "Objective-C", "Java", "Others", "CSS"],
-    datasets: [
-      {
-        label: "languages",
-        backgroundColor: [
-          `rgba(241, 224, 90, ${t})`,
-          `rgb(86, 61, 124, ${t})`,
-          `rgb(227, 76, 38, ${t})`,
-          `rgb(44, 62, 80, ${t})`,
-          `rgb(43, 116, 137, ${t})`,
-          `rgb(86, 61, 124, ${t})`
-        ],
-        borderColor: [
-          "#f1e05a",
-          "#563d7c",
-          "#e34c26",
-          "#2c3e50",
-          "#2b7489",
-          "#563d7c"
-        ],
-        borderWidth: 2.4,
-        data: [6, 6, 4, 3, 3, 2],
-        barThickness: 30
-      }
-    ]
-  },
-  options: {
-    cutoutPercentage: 65,
-    // responsive: false,
-    // scales: {
-    //   yAxes: [
-    //     {
-    //       ticks: {
-    //         beginAtZero: true
-    //       }
-    //     }
-    //   ]
-    // },
-    legend: {
-      display: true,
-      position: "top",
-      align: "center",
-      labels: {
-        boxWidth: 7,
-        padding: 14,
-        usePointStyle: true
-      }
-    },
-    tooltips: {
-      mode: "point",
-      titleFontColor: "black",
-      bodyFontColor: "black",
-      backgroundColor: "#eee"
-    }
-  }
-});
+// let myChart3 = new Chart(ctx3, {
+//   plugins: [
+//     {
+//       beforeInit: function(chart) {
+//         chart.legend.afterFit = function() {
+//           this.height = this.height + 20;
+//         };
+//       }
+//     }
+//   ],
+//   type: "pie",
+
+//   data: {
+//     labels: ["Python", "Swift", "Objective-C", "Java", "Others", "CSS"],
+//     datasets: [
+//       {
+//         label: "languages",
+//         backgroundColor: [
+//           `rgba(241, 224, 90, ${t})`,
+//           `rgb(86, 61, 124, ${t})`,
+//           `rgb(227, 76, 38, ${t})`,
+//           `rgb(44, 62, 80, ${t})`,
+//           `rgb(43, 116, 137, ${t})`,
+//           `rgb(86, 61, 124, ${t})`
+//         ],
+//         borderColor: [
+//           "#f1e05a",
+//           "#563d7c",
+//           "#e34c26",
+//           "#2c3e50",
+//           "#2b7489",
+//           "#563d7c"
+//         ],
+//         borderWidth: 2.4,
+//         data: [6, 6, 4, 3, 3, 2],
+//         barThickness: 30
+//       }
+//     ]
+//   },
+//   options: {
+//     cutoutPercentage: 65,
+//     // responsive: false,
+//     // scales: {
+//     //   yAxes: [
+//     //     {
+//     //       ticks: {
+//     //         beginAtZero: true
+//     //       }
+//     //     }
+//     //   ]
+//     // },
+//     legend: {
+//       display: true,
+//       position: "top",
+//       align: "center",
+//       labels: {
+//         boxWidth: 7,
+//         padding: 14,
+//         usePointStyle: true
+//       }
+//     },
+//     tooltips: {
+//       mode: "point",
+//       titleFontColor: "black",
+//       bodyFontColor: "black",
+//       backgroundColor: "#eee"
+//     }
+//   }
+// });
